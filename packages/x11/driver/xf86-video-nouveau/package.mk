4751bb12626390e7c8106baa7632f61c2bc438c2
Azkali <a.ffcc7@gmail.com>
2021-01-14 20:27:18 +0100

    Fix video-nouveau dependency

--- packages/x11/driver/xf86-video-nouveau/package.mk
+++ packages/x11/driver/xf86-video-nouveau/package.mk
@@ -5,7 +5,7 @@ PKG_ARCH="any"
 PKG_SECTION="x11/driver"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
-PKG_DEPENDS_TARGET="toolchain util-macros"
+PKG_DEPENDS_TARGET="toolchain util-macros xorg-server"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/driver/$PKG_NAME-$PKG_VERSION.tar.bz2"
 
 PKG_IS_ADDON="no"
