32ea644dd4bbdafcb5e40d72df9a49105773d26d
Ronald Brown <rbrown4014@yahoo.com>
2021-01-21 01:42:58 -0800

    Forgot about these

--- projects/L4T/devices/Switch/packages/switch-bootloader/package.mk
+++ projects/L4T/devices/Switch/packages/switch-bootloader/package.mk
@@ -30,14 +30,10 @@ PKG_AUTORECONF="no"
 makeinstall_target() {
   mkdir -p $INSTALL/usr/share/bootloader/boot
   mkimage -A arm -T script -O linux -d $PKG_DIR/assets/boot.txt $BUILD/$PKG_NAME-$PKG_VERSION/boot.scr
-  mkimage -A arm -T script -O linux -d $PKG_DIR/assets/boot_uart.txt $BUILD/$PKG_NAME-$PKG_VERSION/boot_uart.scr
-  mkimage -A arm -T script -O linux -d $PKG_DIR/assets/boot_usb.txt $BUILD/$PKG_NAME-$PKG_VERSION/boot_usb.scr
 
   cp -PRv $PKG_DIR/assets/splash.bmp $INSTALL/usr/share/bootloader/boot/splash.bmp
   cp -PRv $PKG_DIR/assets/00-Lakka.ini $INSTALL/usr/share/bootloader/boot/00-Lakka.ini
   cp -PRv $BUILD/$PKG_NAME-$PKG_VERSION/boot.scr $INSTALL/usr/share/bootloader/boot/boot.scr
-  cp -PRv $BUILD/$PKG_NAME-$PKG_VERSION/boot_uart.scr $INSTALL/usr/share/bootloader/boot/boot_uart.scr
-    cp -PRv $BUILD/$PKG_NAME-$PKG_VERSION/boot_usb.scr $INSTALL/usr/share/bootloader/boot/boot_usb.scr
   cp -PRv $BUILD/switch-boot/coreboot.rom $INSTALL/usr/share/bootloader/boot/coreboot.rom
 }
 
